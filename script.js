e5dc678e58b6257f6ecf0f6e96f319c3e1cb4da4
kihBootCamp <kimberleyheuer@hotmail.com>
2020-11-27 17:02:40 -0600

    setting answers on screen

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 var btnStartStop = $("#btn-startstop");
 
-var uList = $("#ulist")
+var uList = $("#ulist");
+var ansList = $("#anslist");
 
 
 
 btnStartStop.on("click", function(event) {
     console.log($("#btn-startstop").html);
 
-    // if ($("#btn-startstop").html() === "Start")
-    // $("#btn-startstop").html("Stop");
-    // else
-    // $("#btn-startstop").html("Start");
-
-    if (btnStartStop.html() === "Start")
-        $btnStartStop.html("Stop");
+    if (btnStartStop.html() === "Start") {
+        btnStartStop.html("Stop");
+        add_string_questions();
+    }
     else
-        $btnStartStop.html("Start");
+        btnStartStop.html("Start");
 })
 
 //
 // Each category has it's own list of questions
 
 init_main_menu();
+
 // initialize main menu
 function init_main_menu() {
 
     uList.empty();
-    uList.append($("<li id='quest-string'>Is it a string?</li>"));
+    uList.append($("<li id='quest-string'>Is it a string or not?</li>"));
     uList.append($("<li id='quest-array'>Arrays forever!</li>"));
     uList.append($("<li id='quest-dotnot'>Dot not, what?</li>"));  
     
@@ -40,9 +39,36 @@ function init_main_menu() {
 }
 
 function add_string_questions() {
-    uList.empty();
+    
     $("h3").html("Category is String");
     $("h4").html("True or False: The first position of any string is 1?");
+
+    uList.empty();
+    uList.append($("<li class='no-bullet'><button type='button' class='btn btn-dark'  id='btn-true'>True</button></li>"));
+    uList.append($("<li id='quest-false' class='no-bullet'><button type='button' class='btn btn-dark'  id='btn-false'>False</button></li>"));
+
+    var btnTrue = $("#btn-true");
+    btnTrue.on("click", function(){
+        check_answer("t", "t");
+    });
+}
+
+
+// var falseButton = $("#false-button");
+
+// trueButton.on("click", check_answer("t", "f"));
+
+
+function check_answer(user_input, correct_answer){
+    var ansPanel = $("#ans-panel");
+    var ansTimer = $("#ans-timer");
+
+    if (user_input === correct_answer) {
+        ansPanel.text("Correct!")
+    }
+    else {
+        ansPanel.text("Nope!")
+    }
 }
 
 function add_array_questions() {
