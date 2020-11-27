a8f7e1eedf4d8fb0eeb8209adf38127dd1ffea13
kihBootCamp <kimberleyheuer@hotmail.com>
2020-11-27 17:34:53 -0600

    working on timer

--- script.js
+++ script.js
@@ -11,9 +11,12 @@ btnStartStop.on("click", function(event) {
     if (btnStartStop.html() === "Start") {
         btnStartStop.html("Stop");
         add_string_questions();
+        set_timer();
     }
-    else
+    else {
         btnStartStop.html("Start");
+        init_main_menu();
+    }
 })
 
 //
@@ -23,6 +26,9 @@ init_main_menu();
 
 // initialize main menu
 function init_main_menu() {
+
+    $("h3").html("The questions will come from the following categories");
+    $("h4").html("You have 2 minutes after clicking Start");
 
     uList.empty();
     uList.append($("<li id='quest-string'>Is it a string or not?</li>"));
@@ -49,14 +55,14 @@ function add_string_questions() {
 
     var btnTrue = $("#btn-true");
     btnTrue.on("click", function(){
-        check_answer("t", "t");
+        check_answer("t", "f");
+    });
+    var btnFalse = $("#btn-false");
+    btnFalse.on("click", function(){
+        check_answer("f", "f");
     });
 }
 
-
-// var falseButton = $("#false-button");
-
-// trueButton.on("click", check_answer("t", "f"));
 
 
 function check_answer(user_input, correct_answer){
@@ -77,4 +83,41 @@ function add_array_questions() {
 
 function add_dotnot_questions() {
     alert("dot not hi");
+}
+
+function set_timer() {
+    // Thank you W3 Schools.com!
+    //https://www.w3schools.com/howto/howto_js_countdown.asp
+
+    // Set the date we're counting down to + 1 minute
+
+    var countDownDate = new Date().getTime() + (1 * 60000);
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+        
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+        
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+    // Output the result in an element with id="demo"
+    // document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    // + minutes + "m " + seconds + "s ";
+    $("#timer-display").text(minutes + "m " + seconds + "s ")
+        
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        //document.getElementById("demo").innerHTML = "EXPIRED";
+        $("#timer-display").text("TIME OUT!");
+    }
+    }, 1000);
 }
